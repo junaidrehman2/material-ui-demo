8a8d1d9509d798c174d6d5cad1858a27e4ae673a
junaidrehman2 <junaid@homepass.com>
2018-10-08 00:51:20 +1100

    de

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Header, Footer} from './layouts';
+import 'typeface-roboto';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header />
+       <h1>HI</h1>
+       <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layouts/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default porps => (
+  <div>
+    <h1> Header  </h1>
+  </div>
+)
